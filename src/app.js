bdd73fb38b159d12243b5919d19923aa62906a70
svalessandro <alessandro.siva7545@gmail.com>
2025-05-25 23:25:50 -0300

    última vez

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // <-- carrega variáveis de ambiente
 const express = require('express');
 const cors = require('cors');
 const webhookRoutes = require('./routes/webhook');
@@ -6,11 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/webhook', webhookRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
